3c2ba7e6225441444d97cf855b0ebfbf62d4f8c5
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-05-03 18:02:36 -0500

    Added description of U parameter in mvebnm; working on function simualte_mvebnm_data.

--- R/datasim.R
+++ R/datasim.R
@@ -1,18 +1,33 @@
-# Simulate n data points from the "extreme deconvolution" mixture
-# model: each data point is drawn from a mixture of normals in which
-# each normal mixture component k has zero mean and covariance matrix
-# S + U[[k]]. Input argument w specifies the mixture weights.
+#' @title Simulate Data from Multivariate Normal Means Model
 #'
+#' @description Simulate n data points from the multivariate normal
+#'   means model. See \code{\link{mvebnm}} for the model definition.
+#'
+#' @param w A numeric vector of length k specifying the prior mixture
+#'   weights. All entries must be non-negative, but need not sum to 1;
+#'   the mixture weights are automatically normalized to sum to 1.
+#'
+#' @param U A list of length k specifying the covariance matrices in
+#'   the mixture-of-multivariate-normals prior; list element
+#'   \code{U[[i]]} is the m x m covariance matrix for the ith mixture
+#'   component.
+#' 
+#' @param S The m x m residual covariance matrix.
+#'
+#' @return Describe output here.
+#' 
+#' @seealso \code{\link{mvebnm}}
+#' 
 #' @export
 #' 
-simulate_mvnm_data <- function (n, w, U, S) {
+simulate_ebmvnm_data <- function (n, w, U, S) {
 
   # Get the number of outcomes (m) and the number of mixture
   # components (k).
   m <- nrow(S)
   k <- length(w)
 
-  # Check that all the covariance matrices are s.p.d.
+  # Check that all the (marginal) covariance matrices are s.p.d.
   for (j in 1:k)
     tryCatch(R <- chol(S + U[[j]]),
              error = function (e)
