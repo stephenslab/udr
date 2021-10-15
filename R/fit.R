a03ee79537bfde38f6389ce514fd98a17ee470ba
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-10-14 20:47:29 -0500

    Bumped up version; updated .Rbuildignore.

--- R/fit.R
+++ R/fit.R
@@ -241,7 +241,7 @@ ud_fit <- function (fit, X, control = list(), verbose = TRUE) {
   # Give an overview of the model fitting.
   if (verbose) {
     cat(sprintf("Performing Ultimate Deconvolution on %d x %d matrix ",n,m))
-    cat(sprintf("(udr 0.3-89, \"%s\"):\n",control$version))
+    cat(sprintf("(udr 0.3-90, \"%s\"):\n",control$version))
     if (is.matrix(fit$V))
       cat("data points are i.i.d. (same V)\n")
     else
