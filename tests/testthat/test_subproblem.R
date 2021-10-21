72733f380bf2a092a3c2babc4e08fa61b1e2a777
yunqiyang0215 <yunqiyang@hsph.harvard.edu>
2021-10-21 10:34:56 -0500

    more tests for subproblmes

--- tests/testthat/test_subproblem.R
+++ tests/testthat/test_subproblem.R
@@ -1,5 +1,102 @@
 
-test_that("unconstrained ED general vs. ED iid", {
+test_that("unconstrained FA increases loglikelihood", {
+  
+  # Simulate data
+  seed <- 1
+  n <- 500
+  U.true <- diag(2)
+  V <- diag(2)
+  X <- simulate_one_component(seed, n, U.true, V)
+
+  # Initialization
+  p <- runif(n)
+  maxiter <- 10
+  U <- sim_unconstrained(2)
+  logliks <- rep(NA, maxiter)
+  
+  for (i in 1:maxiter){
+    Unew <- update_prior_covariance_unconstrained_fa(X, U, p)
+    U <- Unew
+    logliks[i] <- loglik_weighted_single_component(X, U, V, p)
+  }
+  expect_nondecreasing(logliks)
+})
+
+test_that("unconstrained ED_iid increases loglikelihood", {
+  # Simulate data
+  seed <- 1
+  n <- 500
+  U.true <- diag(2)
+  V <- diag(2)
+  X <- simulate_one_component(seed, n, U.true, V)
+
+  # Initialization
+  p <- runif(n)
+  maxiter <- 10
+  U <- sim_unconstrained(2)
+  logliks <- rep(NA, maxiter)
+  
+  for (i in 1:maxiter){
+    Unew <-  update_prior_covariance_ed_iid(X, U, V, p)
+    U <- Unew
+    logliks[i] <- loglik_weighted_single_component(X, U, V, p)
+  }
+  expect_nondecreasing(logliks)
+})
+
+test_that("scaled FA_general increases loglikelihood", {
+  
+  # Simulate data
+  seed <- 1
+  n <- 500
+  U0 <- matrix(c(1,0,0,0), ncol = 2, nrow = 2)
+  V.g <- simplify2array(replicate(n, diag(2), simplify=FALSE))
+  X = simulate_one_component(seed, n, U0, V)
+  
+  # Initialization
+  maxiter <- 10
+  U <- create_scaled_matrix_struct(X, U0)
+  r <- sum(eigen(U$U0)$values > 1e-15)
+  minval <- 0
+  p <- runif(n)
+  logliks <- rep(NA, maxiter)
+  
+  for (i in 1: maxiter){
+    s <- update_prior_covariance_scaled_fa_general(X, U$U0, V.g, p, U$s, r)
+    Unew <- update_prior_covariance_scaled_struct(U, s)
+    logliks[i] <- loglik_weighted_single_component(X, U$mat, V.g, p)
+    U <- Unew
+}
+  expect_nondecreasing(logliks)
+})
+
+test_that("rank1 FA_iid increases loglikelihood", {
+  
+  # Simulate data
+  seed <- 1
+  n <- 500
+  U.true <- matrix(c(1,0,0,0), ncol = 2, nrow = 2)
+  V <- diag(2)
+  X <- simulate_one_component(seed, n, U.true, V)
+  
+  # Initialization
+  maxiter <- 10
+  U <- sim_rank1(2)
+  U <- create_rank1_matrix_struct(X, U)
+  p <- runif(n)
+  logliks <- rep(NA, maxiter)
+  
+  for (i in 1: maxiter){
+    vec <- update_prior_covariance_rank1_fa_iid(X, U$vec, p)
+    Unew <- update_prior_covariance_rank1_struct(U, vec)
+    logliks[i] <- loglik_weighted_single_component(X, U$mat, V, p)
+    U <- Unew
+}
+  expect_nondecreasing(logliks)
+})
+
+
+test_that("unconstrained ED_general vs. ED_iid", {
   
   set.seed(1)
   # Simulate data
