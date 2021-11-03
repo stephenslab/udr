ebdf14910e0e61612e72ff3330d43cdb270e770e
yunqiyang <yunqiyang@uchicago.edu>
2021-11-03 16:18:55 -0500

    fix mistakes scale fa-iid

--- R/update_prior_subproblems.R
+++ R/update_prior_subproblems.R
@@ -400,7 +400,7 @@ update_prior_covariance_scaled_fa_iid <- function(X, U0, p, s, r){
   }
   
   if (r ==1){
-    s = sum(p*B)/sum(p)
+    s = sum(p*unlist(B))/sum(p)
   }else{
     s = sum(trB*p)/(sum(p)*r)
   }
