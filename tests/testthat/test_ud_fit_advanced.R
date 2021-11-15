bbbbc6584fad1b8669b8b0fe104abb3a4ad82658
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-11-15 09:03:26 -0600

    Added some tests for ted updates.

--- tests/testthat/test_ud_fit_advanced.R
+++ tests/testthat/test_ud_fit_advanced.R
@@ -1,8 +1,7 @@
 context("ud_fit_advanced")
 
 test_that(paste("R and C++ implementations of advanced model fitting ",
-                "interface produce the same resultud_fit produce same",
-                "result"),{
+                "interface produce the same result for i.i.d. case"),{
 
   # Simulate data.
   set.seed(1)
@@ -12,14 +11,11 @@ test_that(paste("R and C++ implementations of advanced model fitting ",
 
   # Perform 4 EM updates.
   fit <- ud_init(X)
-  capture.output(fit <- ud_fit(fit,control = list(maxiter = 4)))
+  control <- list(scaled.update = "none",rank1.update = "none",
+                  unconstrained.update = "none")
+  capture.output(fit <- ud_fit(fit,control = c(control,list(maxiter = 4))))
 
   # Update the responsibilities matrix.
-  control <- list(scaled.update = "none",rank1.update = "none")
-  control1 <- control
-  control2 <- control
-  control1$version <- "R"
-  control2$version <- "Rcpp"
   fit1 <- compute_posterior_probs(fit,version = "R")
   fit2 <- compute_posterior_probs(fit,version = "Rcpp")
   expect_equal(fit1,fit2,scale = 1,tolerance = 1e-15)
@@ -34,7 +30,13 @@ test_that(paste("R and C++ implementations of advanced model fitting ",
   fit2 <- update_resid_covariance(fit2,version = "Rcpp")
   expect_equal(fit1,fit2,scale = 1,tolerance = 1e-15)
 
-  # Update the prior covariance matrices, U.
+  # Update the prior covariance matrices, U, using the TED algorithm.
+  control <- list(scaled.update = "none",rank1.update = "ted",
+                  unconstrained.update = "ted")
+  control1 <- control
+  control2 <- control
+  control1$version <- "R"
+  control2$version <- "Rcpp"
   updates1 <- assign_prior_covariance_updates(fit1,control1)$covupdates
   updates2 <- assign_prior_covariance_updates(fit2,control2)$covupdates
   fit1 <- update_prior_covariances(fit1,updates1)
@@ -42,3 +44,44 @@ test_that(paste("R and C++ implementations of advanced model fitting ",
   expect_equal(fit1,fit2,scale = 1,tolerance = 1e-14)
 })
 
+test_that(paste("R and C++ implementations of advanced model fitting ",
+                "interface produce the same result for non-i.i.d. case"),{
+
+  # Simulate data.
+  set.seed(1)
+  n   <- 100
+  dat <- simulate_ud_data_2d(n)
+  X   <- dat$X
+
+  # Perform 4 EM updates.
+  fit <- ud_init(X,V = rep(list(dat$V),n))
+  control <- list(scaled.update = "none",rank1.update = "none",
+                  unconstrained.update = "none")
+  capture.output(fit <- ud_fit(fit,control = c(control,list(maxiter = 4))))
+
+  # Update the responsibilities matrix.
+  control1 <- control
+  control2 <- control
+  control1$version <- "R"
+  control2$version <- "Rcpp"
+  fit1 <- compute_posterior_probs(fit,version = "R")
+  fit2 <- compute_posterior_probs(fit,version = "Rcpp")
+  expect_equal(fit1,fit2,scale = 1,tolerance = 1e-15)
+  
+  # Update the mixture weights.
+  fit1 <- update_mixture_weights(fit1)
+  fit2 <- update_mixture_weights(fit2)
+  expect_equal(fit1,fit2,scale = 1,tolerance = 1e-15)
+
+  # Attempting to perform the TED updates should result in an error.
+  control <- list(scaled.update = "none",rank1.update = "ted",
+                  unconstrained.update = "ted")
+  control1 <- control
+  control2 <- control
+  control1$version <- "R"
+  control2$version <- "Rcpp"
+  updates1 <- assign_prior_covariance_updates(fit1,control1)$covupdates
+  updates2 <- assign_prior_covariance_updates(fit2,control2)$covupdates
+  expect_error(update_prior_covariances(fit1,updates1))
+  expect_error(update_prior_covariances(fit2,updates2))
+})
