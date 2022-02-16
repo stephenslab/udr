6b067cb04b4e9bcf75218e60ea20d0bac3ee6c91
yunqiyang0215 <yunqiyang@hsph.harvard.edu>
2022-02-15 21:40:33 -0600

    minor change

--- R/ud_autocv.R
+++ R/ud_autocv.R
@@ -1,8 +1,8 @@
 
-
 # Function to perform n-fold cross-validation
 #' @param X: n by R data matrix
 #' @param V: R by R residual covariance matrix
+#' @param nfold: An integer, the number of folds used in CV. 
 #' @param n_unconstrained: An integer, the number of unconstrained matrix to fit
 #' @param n_rank1: An integer, the number of rank1 matrix to fit
 ud_cv = function(X, V, nfold, n_unconstrained, n_rank1, control, verbose){
@@ -21,8 +21,8 @@ ud_cv = function(X, V, nfold, n_unconstrained, n_rank1, control, verbose){
     # fit model 
     fit1 <- ud_init(X.train, n_unconstrained = n_unconstrained, n_rank1 = n_rank1, U_scaled = NULL, V = V)
     fit2 <- ud_fit(fit1,control = list(unconstrained.update = "ted", rank1.update = "ted",
-                                       resid.update = 'none', maxiter = control$maxiter, tol = control$tol),verbose = verbose)
-  
+                                       resid.update = 'none', maxiter = control$maxiter, tol = 1e-5),verbose = verbose)
+    
     U <- lapply(fit2$U,function (e) "[["(e,"mat"))
     U <- simplify2array(U)
     loglik.test[i] <- udr:::loglik_ud(X.test, fit2$w, U, fit2$V)
@@ -30,10 +30,12 @@ ud_cv = function(X, V, nfold, n_unconstrained, n_rank1, control, verbose){
   return(avg.loglik = mean(loglik.test)/size)
 }
 
+
 # Function to select best k components by cross-validation. At least, one of ku and k1 
 # should be specified by the user. 
 #' @param X: n by R data matrix
 #' @param V: residual covariance matrix
+#' @param nfold: An integer, the number of folds used in CV. 
 #' @param ku: An integer or a list of integers specifying the number of
 #' unconstrained components to experiment with.
 #' @param k1: An integer or a list of integers specifying the number of
@@ -43,12 +45,10 @@ ud_fit_cv = function(X, V, nfold, ku = 0, k1= 0, control=list(), verbose){
   
   k = max(length(ku), length(k1))
   avg_logliks = c(-Inf, rep(NA, k)) # store average loglikelihood under each scenario
-  kmat = matrix(0, nrow = 2, ncol = k) # store the values of ku and k1
+  kmat = matrix(0, nrow = 2, ncol = k) # store the values of ku and k1 under each evaluated scenario
   rownames(kmat) = c("ku", "k1")
   
-  # Modify control list
   control <- modifyList(ud_fit_control_default(),control,keep.null = TRUE)
- 
  
   # Perform CV on different k and evaluate if average log-likelihood 
   # increases. Early stop is available if og-likelihood decreases. 
@@ -67,5 +67,5 @@ ud_fit_cv = function(X, V, nfold, ku = 0, k1= 0, control=list(), verbose){
       break 
     }
   }
-  return(list(ku = n_unconstrained, k1 = n_rank1, scenario = kmat, avg_logliks=  avg_logliks))
+  return(list(ku = n_unconstrained, k1 = n_rank1, scenario = kmat[,1:i], avg_logliks= avg_logliks[2:i]))
 }
