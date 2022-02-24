dd9196dfd695c26602157a1dea7f0726ad861a4d
yunqiyang0215 <yunqiyang@hsph.harvard.edu>
2022-02-24 11:35:20 -0600

    change ku/k1 to k_unconstrained/k_rank1

--- R/ud_autocv.R
+++ R/ud_autocv.R
@@ -4,15 +4,15 @@
 #' @param V: R by R residual covariance matrix
 #' @param nfold: An integer, the number of folds used in CV. 
 #' @param n_unconstrained: An integer, the number of unconstrained matrix to fit
-#' @param n_rank1: An integer, the number of rank1 matrix to fit
+#' @param n_rank1: An integer, the number of rank_rank1 matrix to fit
 cv_single_model = function(X, V, nfold, n_unconstrained, n_rank1, control, verbose){
   
   n = nrow(X)
   size = round(n / nfold)
   loglik.test = rep(NA, nfold)
   
-  for (i in 1:nfold){
     # split data
+  for (i in 1:nfold){
     start = 1+(i-1)*size
     end = ifelse(i==nfold, n, i*size)
     X.test = X[start:end, ]
@@ -20,7 +20,7 @@ cv_single_model = function(X, V, nfold, n_unconstrained, n_rank1, control, verbo
     
     # fit model 
     fit1 <- ud_init(X.train, n_unconstrained = n_unconstrained, n_rank1 = n_rank1, U_scaled = NULL, V = V)
-    fit2 <- ud_fit(fit1,control = list(unconstrained.update = "ted", rank1.update = "ted",
+    fit2 <- ud_fit(fit1,control = list(unconstrained.update = "ted", rank_rank1.update = "ted",
                                        resid.update = 'none', maxiter = control$maxiter, tol = 1e-5),verbose = verbose)
     
     U <- lapply(fit2$U,function (e) "[["(e,"mat"))
@@ -31,39 +31,39 @@ cv_single_model = function(X, V, nfold, n_unconstrained, n_rank1, control, verbo
 }
 
 
-# Function to select best k components by cross-validation. At least, one of ku and k1 
+# Function to select best k components by cross-validation. At least, one of k_unconstrained and k_rank1 
 # should be specified by the user. 
 #' @param X: n by R data matrix
 #' @param V: residual covariance matrix
 #' @param nfold: An integer, the number of folds used in CV. 
-#' @param ku: An integer or a vector of integers specifying the number of
+#' @param k_unconstrained: An integer or a vector of integers specifying the number of
 #' unconstrained components to experiment with.
-#' @param k1: An integer or a vector of integers specifying the number of
+#' @param k_rank1: An integer or a vector of integers specifying the number of
 #' rank1 components to experiment with. 
 
-ud_fit_cv = function(X, V, nfold, ku = 0, k1= 0, control=list(), verbose){
+ud_fit_cv = function(X, V, nfold, k_unconstrained = 0, k_rank1= 0, control=list(), verbose){
   
-  if (length(ku) == 1 & length(k1) == 1)
-    if (ku == 0 & k1 == 0)
-      stop("At least \"ku\" or \"k1\" has to be specified")
+  if (length(k_unconstrained) == 1 & length(k_rank1) == 1)
+    if (k_unconstrained == 0 & k_rank1 == 0)
+      stop("At least \"k_unconstrained\" or \"k_rank1\" has to be specified")
   
-  if ((length(ku) > 1) & (length(k1) > 1))
-    if (length(ku) != length(k1))
-      stop("\"ku\" and \"k1\" should have the same length if they are both vectors")
+  if ((length(k_unconstrained) > 1) & (length(k_rank1) > 1))
+    if (length(k_unconstrained) != length(k_rank1))
+      stop("\"k_unconstrained\" and \"k_rank1\" should have the same length if they are both vectors")
   
-  k = max(length(ku), length(k1))
+  k = max(length(k_unconstrained), length(k_rank1))
   
   avg_logliks = c(-Inf, rep(NA, k)) # store average loglikelihood under each scenario
-  kmat = matrix(0, nrow = 2, ncol = k) # store the values of ku and k1 under each evaluated scenario
-  rownames(kmat) = c("ku", "k1")
+  kmat = matrix(0, nrow = 2, ncol = k) # store the values of k_unconstrained and k_rank1 under each evaluated scenario
+  rownames(kmat) = c("k_unconstrained", "k_rank1")
   
   control <- modifyList(ud_fit_control_default(),control,keep.null = TRUE)
  
   # Perform CV on different k and evaluate if average log-likelihood 
   # increases. Early stop is available if og-likelihood decreases.
   for (i in 1:k){
-    n_unconstrained = ifelse(i > length(ku), ku, ku[i])
-    n_rank1 = ifelse(i > length(k1), k1, k1[i])
+    n_unconstrained = ifelse(i > length(k_unconstrained), k_unconstrained, k_unconstrained[i])
+    n_rank1 = ifelse(i > length(k_rank1), k_rank1, k_rank1[i])
     kmat[,i] = c(n_unconstrained, n_rank1) # store n_unconstrained and n_rank1 in curr iteration
     
     # Perform CV
@@ -76,5 +76,5 @@ ud_fit_cv = function(X, V, nfold, ku = 0, k1= 0, control=list(), verbose){
       break 
     }
   }
-  return(list(ku = n_unconstrained, k1 = n_rank1, scenario = kmat[,1:i], avg_logliks= avg_logliks[2:(i+1)]))
+  return(list(n_unconstrained = n_unconstrained, n_rank1 = n_rank1, scenario = kmat[,1:i], avg_logliks= avg_logliks[2:(i+1)]))
 }
