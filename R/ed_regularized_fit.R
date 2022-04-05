25140558b17dc582fee5bfc40da55b3fa567cb84
yunqiyang0215 <yunqiyang@hsph.harvard.edu>
2022-04-05 16:39:52 -0500

    modify regularized ED

--- R/ed_regularized_fit.R
+++ R/ed_regularized_fit.R
@@ -3,7 +3,7 @@
 #' prior on U. U \sim W_R^{-1}(\Phi, \nu). Here \nu = R-1 and \Phi = (\nu+R+1)*I.
 #' The derivation for U is available in overleaf eq.(33).
 #' https://www.overleaf.com/read/vrgwpskkhbpj
-ed_reg_iid <- function(X, U, p, Phi = 2*ncol(U)*diag(ncol(U)), nu = diag(ncol(U)) - 1){
+ed_reg_iid <- function(X, U, p, Phi = (nu+ncol(U)+1)*diag(ncol(U)), nu = diag(ncol(U)) - 1){
   m  <- ncol(X)
   I  <- diag(m)
   A  <- solve(U + I,U)
@@ -13,13 +13,26 @@ ed_reg_iid <- function(X, U, p, Phi = 2*ncol(U)*diag(ncol(U)), nu = diag(ncol(U)
 }
 
 
+#' Function to compute log posterior. 
+compute_log_posterior <- function(X, w, U, V, Phi, nu){
+  log_prior <- 0
+  K <- length(w)
+  for (k in 1:K){
+    log_prior = log_prior + dinvwishart((U[,,k]+t(U[,,k]))/2, nu = nu, S = Phi, log=TRUE)
+  }
+  loglik <- loglik_ud_iid_helper(X, w, U, V) 
+  log_posterior <- loglik + log_prior
+  return(log_posterior)
+}
+
+
 #' Function to fit regularized ED. 
 #' @param U: a 3d array contains k prior covariance matrix
-em_fit_regularized_ed <- function(X, w, U, V, maxiter, tol, tol.lik){
+em_fit_regularized_ed <- function(X, w, U, V, Phi, nu, maxiter, tol, tol.lik){
   # initialize progress to store progress at each iteration
   
-  progress <- as.data.frame(matrix(0, maxiter,4))
-  names(progress) <- c("iter","loglik","delta.w","delta.u")
+  progress <- as.data.frame(matrix(0, maxiter,5))
+  names(progress) <- c("iter","loglik", "log_posterior", "delta.w","delta.u")
   progress$iter <- 1:maxiter
 
   # Get the number of samples (n) and the number of mixture components (k)
@@ -36,17 +49,20 @@ em_fit_regularized_ed <- function(X, w, U, V, maxiter, tol, tol.lik){
     # M-step: 
     # update covariance matrix 
     for (j in 1:k){
-      U[,,j] = ed_reg_iid(X, U[,,j], P[,j])
+      U[,,j] = ed_reg_iid(X, U[,,j], P[,j], Phi, nu)
     }
     # update mixture weight
     w = colSums(P)/n
     # Compute log-likelihood.
     loglik = loglik_ud_iid_helper(X, w, U, V)
+    log_posterior = compute_log_posterior(X, w, U, V, Phi, nu)
+
   
     dw <- max(abs(w - w0))
     dU <- max(abs(U - U0))
     dloglik <- loglik - loglik0
     progress[iter,"loglik"]  <- loglik
+    progress[iter, "log_posterior"] <- log_posterior
     progress[iter,"delta.w"] <- dw 
     progress[iter,"delta.u"] <- dU 
     
